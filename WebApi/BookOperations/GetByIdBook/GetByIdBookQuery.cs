2823e41c945fde392c00f8bfebff237f52a0bc24
atac5 <atac5@DESKTOP-MP5LIA2>
2022-08-11 13:29:48 +0300

    Add project files.

---
+++ WebApi/BookOperations/GetByIdBook/GetByIdBookQuery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Common;
+using WebApi.DBOperations;
+
+namespace WebApi.BookOperations.GetByIdBookQuery
+{
+    public class GetByIdBookQuery
+    {
+        private readonly BookStoreDbContext _dbContext;
+
+        public GetByIdBookQuery(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public BooksViewModel Handle(int id)
+        {
+            var book = _dbContext.Books.Where(book => book.Id == id).SingleOrDefault();
+            if(book is null)
+            {
+                throw new InvalidOperationException("Kitap mevcut değil");
+                
+            }
+            BooksViewModel vm = new BooksViewModel();
+
+            vm.Title = book.Title;
+            vm.Genre = ((GenreEnum)book.GenreId).ToString();
+            vm.PublishDate = book.PublishDate.Date.ToString("dd/MM/yyy");
+            vm.PageCount = book.PageCount;
+            
+            return vm;
+        }
+
+        public class BooksViewModel
+        {
+            public string Title { get; set; }
+            public int PageCount { get; set; }
+            public string PublishDate { get; set; }
+            public string Genre { get; set; }
+        }
+
+    }
+}
---
+++ WebApi/Common/GenreEnum.cs
@@ -0,0 +1,10 @@
+namespace WebApi.Common
+{
+
+    public enum GenreEnum
+    {
+        Personal_Growth = 1,
+        Science_Fiction,
+        Noval
+    }
+}
